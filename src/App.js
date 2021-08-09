cd204dc959f33526f3316e4717450ea05b1700b3
Kervintz Noel <kervintz84@hotmail.com>
2021-08-09 16:50:51 -0400

    section 4 - passing props

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 
 class App extends Component {
@@ -23,6 +24,7 @@ class App extends Component {
         <div className="App">
           <Navbar title="GitHub Finder" icon="fab fa-github"/>
           <div className="container">
+            <Search />
             <Users loading={this.state.loading} users={this.state.users}/>
           </div>
         
--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -4,9 +4,7 @@ import spinner from './spinner.gif'
 
 const Spinner = () => {
   return (
-    <>
       <img src={spinner} alt="Loading..." style={{ width: '200px', margin: auto, display: "block"}} />
-    </>
   )
 }
 
